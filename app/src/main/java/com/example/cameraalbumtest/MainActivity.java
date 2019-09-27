5ce38b8ceff2bd79fd758e112fdb242b0f8502cf
guoxw <xuewei_g@163.com>
2019-09-27 10:19:14 +0800

    IDE test

--- app/src/main/java/com/example/cameraalbumtest/MainActivity.java
+++ app/src/main/java/com/example/cameraalbumtest/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
